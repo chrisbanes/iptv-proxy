525589c9990301d143824d63ee300806a567bc06
Chris Banes <chris@banes.me>
2024-04-16 19:50:22 +0100

    Use my xtream-proxy fork

--- pkg/xtream-proxy/xtream-proxy.go
+++ pkg/xtream-proxy/xtream-proxy.go
@@ -26,7 +26,7 @@ import (
 	"strconv"
 
 	"github.com/pierre-emmanuelJ/iptv-proxy/pkg/config"
-	xtream "github.com/tellytv/go.xtream-codes"
+	xtream "github.com/chrisbanes/go.xtream-codes"
 )
 
 const (
